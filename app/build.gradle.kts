dee0fdd9d86fade153f45cc8dfa43c354ec15b17
zaraiRamirezVega <1518103@senati.pe>
2025-03-03 18:46:40 -0500

    change sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.contactos"
-        minSdk = 34
+        minSdk = 33
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
